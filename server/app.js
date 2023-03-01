a3ff19bd16f04a07c4513969b0059b2f052e41e1
minha sohn <codeminhasohn@gmail.com>
2023-03-01 22:20:49 +0900

    [CHORE] API 주소 수정

--- server/app.js
+++ server/app.js
@@ -1,12 +1,12 @@
-import createError from "http-errors";
-import express from "express";
-import cors from "cors";
-import path from "path";
-import { fileURLToPath } from "url";
-import cookieParser from "cookie-parser";
-import logger from "morgan";
+import createError from 'http-errors';
+import express from 'express';
+import cors from 'cors';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
 
-import indexRouter from "./routes/index.js";
+import indexRouter from './routes/index.js';
 
 const app = express();
 
@@ -15,17 +15,17 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
-app.use(logger("dev"));
+app.use(logger('dev'));
 app.use(express.json());
-app.use(cors({ credentials: true, origin: "http://localhost:3002" }));
+app.use(cors({ credentials: true, origin: 'http://localhost:3002' }));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use("/", indexRouter);
+app.use('/api', indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -36,11 +36,11 @@ app.use(function (req, res, next) {
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render("error");
+  res.render('error');
 });
 
 export default app;
